9571a4524148318327d6ea5ced7d269d00687236
Ahmed <atomcgarry@gmail.com>
2022-01-18 16:04:09 +0000

    validation for name email and phone number

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,9 +2,26 @@ import React, { ReactElement } from "react";
 import RadioButton from "../FormComponents/RadioButton/RadioButton";
 import SimpleInput from "../FormComponents/SimpleInput/SimpleInput";
 import "./Form.css";
-
+import { useState } from "react";
 
 const Form: React.FC = (): ReactElement => {
+  const [emailValition, setEmailValidation] = useState<boolean>(true);
+  const [nameValition, setNameValidation] = useState<boolean>(true);
+  const [phoneValition, setPhoneValidation] = useState<boolean>(true);
+
+  const validMail = (email: string): boolean => {
+    return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(
+      email
+    );
+  };
+
+  const validName = (name: string):boolean => {
+    return /^([\w]{3,})+\s+([\w\s]{3,})+$/i.test(name)
+  }
+
+  const validPhone = (number: string): boolean => {
+    return /^(\(?(0|\+44)[1-9]{1}\d{1,4}?\)?\s?\d{3,4}\s?\d{3,4})$/.test(number)
+  }
 
   return (
     <div className="form">
@@ -15,16 +32,25 @@ const Form: React.FC = (): ReactElement => {
             text="Name"
             placeholder="Ahmed McGarry"
             inputWidth="small"
+            validation={nameValition}
+            setValidation={setNameValidation}
+            validator={validName}
           />
           <SimpleInput
             text="Email"
             placeholder="atomcgarry@gmail.com"
             inputWidth="small"
+            validation={emailValition}
+            setValidation={setEmailValidation}
+            validator={validMail}
           />
           <SimpleInput
             text="Phone Number"
             placeholder="07907733824"
             inputWidth="small"
+            validation={phoneValition}
+            setValidation={setPhoneValidation}
+            validator={validPhone}
           />
         </div>
       </div>
--- src/components/FormComponents/SimpleInput/SimpleInput.tsx
+++ src/components/FormComponents/SimpleInput/SimpleInput.tsx
@@ -1,23 +1,47 @@
+import React from "react";
 import "./SimpleInput.css";
 
 interface SimpleInputInterface {
   text?: string;
   placeholder: string;
   inputWidth: "small" | "large";
+  validation?: boolean;
+  setValidation?: React.Dispatch<React.SetStateAction<boolean>>;
+  validator?: (email:string) => boolean
 }
 const SimpleInput: React.FC<SimpleInputInterface> = ({
   text,
   placeholder,
   inputWidth,
+  validation,
+  setValidation,
+  validator
 }) => {
-  
+
+  const validateField = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input: string = e.target.value
+
+    if (validator && setValidation) {
+      const isValid: boolean = validator(input)
+      setValidation(isValid)
+    }
+  }
+
   return (
     <div className="simple-input-container">
-      <p>{text}</p>
+      <div className="simple-input-header">
+        <p>{text}</p>
+        {validation === false && (
+          <p style={{ color: "red", opacity: "50%", fontSize: "14px" }}>
+            *{text} is incorrect, please check
+          </p>
+        )}
+      </div>
       <input
         placeholder={placeholder}
         style={{ width: inputWidth === "small" ? "450px" : "920px" }}
         autoComplete="none"
+        onChange={(e) => validateField(e)}
       />
     </div>
   );
